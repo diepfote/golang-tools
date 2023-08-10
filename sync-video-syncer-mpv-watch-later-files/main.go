edbd3763d1ade1e8371abfe0dfe0040fdf8de0ec
Florian Sorko <florian.begusch@gmail.com>
2023-08-10 07:12:41 +0000

    Finish minimal POC

--- sync-video-syncer-mpv-watch-later-files/main.go
+++ sync-video-syncer-mpv-watch-later-files/main.go
@@ -9,10 +9,47 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
-	"path"
+	"runtime"
 	"strconv"
 	"strings"
 )
+
+func stripVideoFolder(file, videosFolder string) string {
+	// fmt.Printf("file: %s, videosFolder: %s\n", file, videosFolder)
+	return strings.Split(file, videosFolder)[1]
+}
+
+func copySyncFile(localVideosFolder, localFile, localMd5sumStr, localMpvWatchLaterDir, remoteVideosFolder, remoteFile, remoteMd5sumStr, remoteSyncMpvWatchLaterDir string) bool {
+
+	// startTime := getStartTime(localHome, ".config/mpv/watch_later/7B76A69ECB27D3D80B4B96241C1E31EA")
+	// startTime := getStartTime(localHome, ".local/state/mpv/watch_later/85C7E7264F3A4583BD74B2AB59E6C48B")
+	// fmt.Printf("%f\n", startTime)
+
+	strippedLocalFile := stripVideoFolder(localFile, localVideosFolder)
+	strippedRemoteFile := stripVideoFolder(remoteFile, remoteVideosFolder)
+	if strippedLocalFile != strippedRemoteFile {
+		fmt.Printf("[.] localFile : %s\n", strippedLocalFile)
+		fmt.Printf("[.] remoteFile: %s\n\n", strippedRemoteFile)
+		return false
+	}
+
+	localStartTime := getStartTime(localMpvWatchLaterDir + "/" + localMd5sumStr)
+	remoteStartTime := getStartTime(remoteSyncMpvWatchLaterDir + "/" + remoteMd5sumStr)
+
+	if localStartTime < remoteStartTime {
+		fmt.Printf("[!] override local file. cur local: %f cur remote: %f\n", localStartTime, remoteStartTime)
+		return true
+	}
+	return false
+}
+
+func getMd5Hash(data []byte) string {
+	// fmt.Printf("%s\n", data)
+	md5sum := md5.Sum(data)
+	md5sumStr := hex.EncodeToString(md5sum[:])
+	md5sumStr = strings.ToUpper(md5sumStr)
+	return md5sumStr
+}
 
 func prettyPrintArray(typeOfMessage, nameOfArray string, arr []string) {
 	// snatched from https://stackoverflow.com/a/56242100
@@ -44,31 +81,58 @@ func readContent(filename string) string {
 	return string(bytes)
 }
 
-func getStartTime(home, filename string) float64 {
-	fileToRead := path.Join(home, filename)
-	startTimeStr := strings.Split(readContent(fileToRead), "=")[1]
-	startTimeStr = strings.Split(startTimeStr, "\n")[0]
-	// fmt.Printf("%s\n", startTimeStr)
-	startTime, _ := strconv.ParseFloat(startTimeStr, 8)
-	// fmt.Printf("%s\n", error)
+func getStartTime(filename string) float64 {
+	// fileToRead := path.Join(home, filename)
+	// fmt.Printf("fileToRead: %s\n", filename)
+	content := readContent(filename)
+
+	startTime := 0.0
+	if len(content) > 0 {
+		startTimeStr := strings.Split(content, "=")[1]
+		startTimeStr = strings.Split(startTimeStr, "\n")[0]
+		// fmt.Printf("%s\n", startTimeStr)
+		startTime, _ = strconv.ParseFloat(startTimeStr, 8)
+		// fmt.Printf("%s\n", error)
+	}
 	return startTime
 }
 
 func main() {
-	env_vars := os.Environ()
-	home := ""
-	for _, env_var := range env_vars {
-		// fmt.Printf("env_var: %v\n", env_var)
-		switch {
-		case strings.HasPrefix(env_var, "HOME="):
-			home = strings.Split(env_var, "=")[1]
-		}
-	}
-	// startTime := getStartTime(home, ".config/mpv/watch_later/85C7E7264F3A4583BD74B2AB59E6C48B")
-	startTime := getStartTime(home, ".local/state/mpv/watch_later/85C7E7264F3A4583BD74B2AB59E6C48B")
-	fmt.Printf("%f\n", startTime)
+	// env_vars := os.Environ()
+	// home := ""
+	// for _, env_var := range env_vars {
+	// 	// fmt.Printf("env_var: %v\n", env_var)
+	// 	switch {
+	// 	case strings.HasPrefix(env_var, "HOME="):
+	// 		home = strings.Split(env_var, "=")[1]
+	// 	}
+	// }
 
-	cmd := exec.Command(home+"/Documents/golang/tools/video-syncer/video-syncer", home+"/Videos", "report-files")
+	macVideosFolder := "Movies"
+	macHome := "/Users/florian.sorko"
+	linuxHome := "/home/flo"
+	linuxVideosFolder := "Videos"
+	linuxMpvWatchLaterDir := linuxHome + "/.local/state/mpv/watch_later"
+	linuxSyncMpvWatchLaterDir := macHome + "/Documents/misc/videos/arch-mpv-watch_later"
+	macMpvWatchLaterDir := macHome + "/.config/mpv/watch_later"
+	macSyncMpvWatchLaterDir := macHome + "/Documents/misc/videos/mac-mpv-watch_later"
+
+	localHome := linuxHome
+	localVideosFolder := linuxVideosFolder
+	localMpvWatchLaterDir := linuxMpvWatchLaterDir
+	remoteHome := macHome
+	remoteVideosFolder := macVideosFolder
+	remoteSyncMpvWatchLaterDir := macSyncMpvWatchLaterDir
+	if runtime.GOOS != "linux" {
+		localHome = macHome
+		localVideosFolder = macVideosFolder
+		localMpvWatchLaterDir = macMpvWatchLaterDir
+		remoteHome = linuxHome
+		remoteVideosFolder = linuxVideosFolder
+		remoteSyncMpvWatchLaterDir = linuxSyncMpvWatchLaterDir
+	}
+
+	cmd := exec.Command(localHome+"/Documents/golang/tools/video-syncer/video-syncer", localHome+"/Movies", "report-files")
 	reportedFilesBytes, _ := cmd.Output()
 	reportedFiles := string(reportedFilesBytes)
 	// reportedFiles = strings.ReplaceAll(reportedFiles, "\n", "")
@@ -81,11 +145,23 @@ func main() {
 	for _, file := range splitStr {
 		// fmt.Printf("%s\n", file)
 		// TODO we have to generate checksums for mac and linux -> reverse filename from checksums
-		data := []byte(home + "/Videos/" + file)
-		fmt.Printf("%s\n", data)
-		md5sum := md5.Sum(data)
-		md5sumStr := hex.EncodeToString(md5sum[:])
-		md5sumStr = strings.ToUpper(md5sumStr)
-		fmt.Printf("%s\n", md5sumStr)
+		localData := []byte(localHome + "/" + localVideosFolder + "/" + file)
+		remoteData := []byte(remoteHome + "/" + remoteVideosFolder + "/" + file)
+		localMd5sumStr := getMd5Hash(localData)
+		remoteMd5sumStr := getMd5Hash(remoteData)
+
+		localFile := string(localData)
+		remoteFile := string(remoteData)
+		copySyncFile(localVideosFolder, localFile, localMd5sumStr, localMpvWatchLaterDir, remoteVideosFolder, remoteFile, remoteMd5sumStr, remoteSyncMpvWatchLaterDir)
+		// if _, err := os.Stat(string(localData)); err == nil {
+		// 	fmt.Printf("local: %s\n", string(localData))
+		// 	fmt.Printf("local: %s\n", localMd5sumStr)
+		// 	fmt.Printf("remote: %s\n", string(remoteData))
+		// 	fmt.Printf("remote: %s\n", remoteMd5sumStr)
+		// }
+		// else if os.IsNotExist(err) {
+		// 	// path/to/whatever does *not* exist
+
+		// }
 	}
 }
